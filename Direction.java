98fa8bc88d6d65baaf0234c3ce965fc98db9be54
Hendrik Werner <hendrik.to@gmail.com>
2016-03-13 14:49:27 +0100

    Fix documentation for Direction * Add missing JavaDoc. * Add missing authors.

--- Direction.java
+++ Direction.java
@@ -1,7 +1,11 @@
 package assignment6;
 
 /**
+ * An enumeration of all cardinal directions.
+ *
  * @author Sjaak Smetsers
+ * @author Hendrik Werner // s4549775
+ * @author Jasper Haasdijk // s4449754
  * @version 1.2
  * @date 28-02-2015 An enumeration type for the 4 points of the compass Each
  * constant has 2 (final) int attributes indicating the displacement of each
@@ -13,15 +17,31 @@ public enum Direction {
 
     private final int dx, dy;
 
+    /**
+     * Constructor method.
+     *
+     * @param dx the delta on the x-axis
+     * @param dy the delta on the y-axis
+     */
     private Direction(int dx, int dy) {
         this.dx = dx;
         this.dy = dy;
     }
 
+    /**
+     * Get the delta on the x-axis.
+     *
+     * @return the delta on the x-axis
+     */
     public int GetDX() {
         return dx;
     }
 
+    /**
+     * Get the delta on the y-axis.
+     *
+     * @return the delta on the y-axis
+     */
     public int GetDY() {
         return dy;
     }
