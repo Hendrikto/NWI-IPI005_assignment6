9ff916acf848b2d124850059601e9bd7b2ebdb35
Hendrik Werner <hendrik.to@gmail.com>
2016-03-13 15:10:41 +0100

    Finish Main * Add missing JavaDoc. * Use a 4 by 4 Configuration. * Use logical statement ordering.

--- Main.java
+++ Main.java
@@ -8,12 +8,21 @@ package assignment6;
  * @author Jasper Haasdijk // s4449754
  */
 public class Main {
+    /**
+     * Entry point
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        int[] game = {1, 2, 3, 4, 5, 6, 9, 7, 8};
-
+        int[] game = {
+            1, 2, 4, 8,
+            16, 5, 6, 3,
+            9, 10, 7, 11,
+            13, 14, 15, 12
+        };
         SlidingGame s = new SlidingGame(game);
-        System.out.println(s);
         Solver solver = new Solver(s);
+        System.out.println(s);
         System.out.println(solver.solve());
     }
 }
